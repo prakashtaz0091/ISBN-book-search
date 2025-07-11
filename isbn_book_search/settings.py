58ffd1499fecf15549f521b45491f36bd3dd5a61
prakashtaz0091 <code.ing2468@gmail.com>
2025-07-11 14:46:39 +0545

    render config

--- isbn_book_search/settings.py
+++ isbn_book_search/settings.py
@@ -11,17 +11,29 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+
+# from decouple import config
+import environ
+import os
+# Initialize environment variables
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+# SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")  # for render.com
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
