c0396cdd5bf782d4cfae464a1463dd74c2280d87
prakashtaz0091 <code.ing2468@gmail.com>
2025-07-11 13:48:23 +0545

    config

--- isbn_book_search/settings.py
+++ isbn_book_search/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-r574527tp_gf6a^n8&#fmctyv#73+5#t^jss^cea)zt9*q%!ne'
 SECRET_KEY = config('SECRET_KEY')
 
 
